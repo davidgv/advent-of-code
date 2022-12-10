161c8b928871010748f6e55f63687cb51f259207
David Gutiérrez <buzon@davidgv.com>
2022-12-10 12:41:57 +0100

    Mayus en comentarios

--- 2022/day01.2.py
+++ 2022/day01.2.py
@@ -1,7 +1,7 @@
 with open("d:\dev\\advent-of-code\\2022\\day01-input.txt", "r") as archivo:
 
   lineas = archivo.readlines()
-  elfos_cal = [] # aquí van las calorías totales por elfo
+  elfos_cal = [] # Aquí van las calorías totales por elfo
   suma_tmp = 0
   for linea in lineas:
     if linea == "\n":
@@ -10,7 +10,7 @@ with open("d:\dev\\advent-of-code\\2022\\day01-input.txt", "r") as archivo:
     else:
       suma_tmp += int(linea)
   
-  elfos_cal.sort(reverse=True) # los 3 primeros valores de la lista son los 3 elfos con más calorías
+  elfos_cal.sort(reverse=True) # Los 3 primeros valores de la lista son los 3 elfos con más calorías
   suma_cal = 0
   for i in range(3):
     suma_cal += elfos_cal[i]
