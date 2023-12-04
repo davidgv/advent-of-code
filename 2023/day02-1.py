1094fe3bb671405d797ee17950248dd3b0063652
David Gutiérrez <buzon@davidgv.com>
2023-12-04 12:57:01 +0100

    Refactorización de 2-1

--- 2023/day02-1.py
+++ 2023/day02-1.py
@@ -7,21 +7,18 @@ suma = 0
 
 for cadena in lista_archivo:
   game = re.findall(r"(\d+) (\w+)", cadena)
-  imposible = False
+  posible = True
   for par in game:
-    if par[1] == "red":
-      if int(par[0]) > 12:
-        imposible = True # no puede tener más de 12 cubos rojos
+    if (par[1] == "red") and (int(par[0]) > 12) :
+        posible = False # no puede tener más de 12 cubos rojos
         break
-    if par[1] == "green":
-      if int(par[0]) > 13:
-        imposible = True # no puede tener más de 13 cubos verdes
+    if (par[1] == "green") and (int(par[0]) > 13) :
+        posible = False # no puede tener más de 13 cubos verdes
         break
-    if par[1] == "blue":
-      if int(par[0]) > 14:
-        imposible = True # no puede tener más de 14 cubos azules
+    if (par[1] == "blue") and (int(par[0]) > 14) :
+        posible = False # no puede tener más de 14 cubos azules
         break
-  if not imposible:
+  if posible:
     num_game = re.findall(r"Game (\d+)", cadena)
     suma += int(num_game[0])
 
